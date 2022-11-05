f1edea22a3cd7efda9167219f71402d654008b27
Matt Conway <matthewjosephconway@gmail.com>
2022-11-05 12:29:07 -0700

    Updated Frame Prefab to use depth, width, and thickness

--- Unity/Assets/Script/Frame.cs
+++ Unity/Assets/Script/Frame.cs
@@ -11,7 +11,7 @@ public class Frame : MonoBehaviour
     public GameObject BotFlange;
 
     [Header("Frame Section Attributes")]
-    public float TotatDepth;
+    public float TotalDepth;
 
     public float FlangeWidth;
     public float FlangeThickness;
@@ -20,15 +20,48 @@ public class Frame : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        UpdateSection(TotatDepth, FlangeWidth, FlangeThickness, WebThickness);
+        UpdateSection(TotalDepth, FlangeWidth, FlangeThickness, WebThickness);
     }
 
-    public void UpdateSection(float totatDepth, float flangeWidth, float flangeThickness, float webThickness)
+    public void UpdateSection(float totalDepth, float flangeWidth, float flangeThickness, float webThickness)
     {
         //reset values
-        TotatDepth = totatDepth;
+        TotalDepth = totalDepth;
         FlangeWidth = flangeWidth;
         FlangeThickness = flangeThickness;
         WebThickness = webThickness;
+
+        //update overall scale
+        var scale = transform.localScale;
+        scale.y = TotalDepth;
+        scale.x = FlangeWidth;
+        transform.localScale = scale;
+
+        //update web scale
+        Web.transform.parent = null;
+        var webScale = Web.transform.localScale;
+        webScale.x = webThickness;
+        Web.transform.localScale = webScale;
+        Web.transform.parent = this.transform;
+
+        //update flange scale
+        TopFlange.transform.parent = null;
+        BotFlange.transform.parent = null;
+        var flangeScale = TopFlange.transform.localScale;
+        flangeScale.y = flangeThickness;
+        TopFlange.transform.localScale = flangeScale;
+        BotFlange.transform.localScale = flangeScale;
+        TopFlange.transform.parent = this.transform;
+        BotFlange.transform.parent = this.transform;
+
+        //update flange placement
+        var topPos = TopFlange.transform.localPosition;
+        var botPos = BotFlange.transform.localPosition;
+        var vertAdjust = flangeThickness / totalDepth / 2.00f;
+        Debug.Log(vertAdjust);
+        topPos.y = 0.5f - vertAdjust;
+        botPos.y = -0.5f + vertAdjust;
+        TopFlange.transform.localPosition = topPos; Debug.Log(topPos);
+        BotFlange.transform.localPosition = botPos; Debug.Log(botPos);
     }
 }
